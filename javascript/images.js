b95d561f699870141417106dbf4cfbf21609d1d0
aiert <otxoboy64@gmail.com>
2025-04-03 10:25:04 +0200

    added a images section

--- javascript/images.js
+++ javascript/images.js
@@ -1,14 +1,47 @@
-async function main() {
-    const images = await fetch("https://wger.de/api/v2/exerciseimage/", {
-        headers: {
-            "X-Api-Key": "WnzOrCpuawSpe4EKPUYLIA==HmJyHb3mtzZ2jhsO",
-            "Accept": "application/json"
-        }
-    });
+import { Image } from './clases.js';
+// async function getImages() 
 
-    const result = await images.json()
-
-    console.log(result);
+function extractExerciseName(url) {
+    return url.split('/').pop().replace('.png', '');
 }
 
-main();
+document.addEventListener("DOMContentLoaded", () => {
+
+    async function getImages() {
+
+        const images = await fetch("https://wger.de/api/v2/exerciseimage/", {
+            headers: {
+                "Authorization": "Token d93103f284c50fe1adb30ed74034edb80513c171",
+                "Accept": "application/json"
+            }
+        });
+    
+        const result = await images.json();
+        let image_array = result.results;
+        console.log(image_array);
+    
+        let image_classes = image_array.map(ex => new Image(ex.id, ex.image));
+        console.log(image_classes)
+    
+    
+        const main = document.getElementById("main-images");
+    
+        image_classes.forEach((element, index) => {
+            const image = document.createElement("div");
+            image.classList.add("image-card");
+            let name = extractExerciseName(element.image);
+    
+            image.innerHTML = `
+            <h2>${name}<h2>
+            <img src="${element.image}">`;
+    
+            main.appendChild(image);
+        });
+    
+    }
+
+    getImages();
+});
+
+
+// getImages();
